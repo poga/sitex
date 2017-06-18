2a4ffa195c7dded09e007c6a73f1b7613e9570e8
Poga Po <poga.bahamut@gmail.com>
2017-06-18 22:45:18 +0800

    remove debug log

--- main_router.go
+++ main_router.go
@@ -3,8 +3,6 @@ package main
 import (
 	"net/http"
 
-	"fmt"
-
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -19,7 +17,6 @@ func (main MainRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, headerRouter := range main.headerRouters {
 		handler, _, _ := headerRouter.Lookup(r.Method, r.URL.Path)
 		if handler != nil {
-			fmt.Println("handling header", r.URL.Path)
 			err := headerRouter.Handle(w, r, nil)
 			if err != nil {
 				return
@@ -27,20 +24,17 @@ func (main MainRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Println("handling shadowing")
 	handler, _, _ := main.shadowingRouter.Lookup(r.Method, r.URL.Path)
 	if handler != nil {
 		handler(w, r, nil)
 		return
 	}
 
-	fmt.Println("handling file")
 	err := main.fileServer.ServeHTTP(w, r)
 	if err != nil {
 		return
 	}
 
-	fmt.Println("handling non shadowing")
 	handler, _, _ = main.nonShadowingRouter.Lookup(r.Method, r.URL.Path)
 	if handler != nil {
 		handler(w, r, nil)
